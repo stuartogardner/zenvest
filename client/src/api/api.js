4e6dfa2dba5df8adb4d5c7f32304ebd3352375b9
Stuart Gardner <stuartogardner@gmail.com>
2017-11-25 21:52:46 +0000

    modifying for deploy

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,27 +1,31 @@
 import axios from 'axios';
 
+const api = axios.create({
+    baseURL: process.env.NODE_ENV === "production" ? '/api' : "http://localhost:3000/api"
+  });
+
 const getDeals = function(){
-    return axios.get('http://localhost:3000/api/deals')
+    return api.get('/deals')
     .then(response => response.data)
 }
 
 const getDealById = function(id){
-    return axios.get('http://localhost:3000/api/deals/'+id)
+    return api.get('/deals/'+id)
     .then(response => response.data)
 }
 
 const addDeal = function(dealInfo){
-    return axios.post('http://localhost:3000/api/deals', dealInfo)
+    return api.post('/deals', dealInfo)
     .then(response => response.data)
 }
 
 const updateDeal = function(updatedInfo, dealId){
-    return axios.patch('http://localhost:3000/api/deals/'+dealId, updatedInfo)
+    return api.patch('/deals/'+dealId, updatedInfo)
     .then(response => response.data)
 }
 
 const getInvestors = function(){
-    return axios.get('http://localhost:3000/api/investors')
+    return api.get('/investors')
     .then(response => response.data)
 }
 
